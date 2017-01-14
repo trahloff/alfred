a18270590ec64ba171c332ed2640a20cba8cd1ab
trahloff <tobias.rahloff@de.ibm.com>
2017-01-14 14:03:23 +0100

    code clean-up

--- components/botUtil.js
+++ components/botUtil.js
@@ -1,38 +1,35 @@
+'use strict';
+
+let bot;
+
 function botUtil(b) {
     bot = b;
 }
 
-var bot;
-
-
-botUtil.prototype.getAllUsers = (cb) => {
-    bot.getUsers().then(function(data) {
-        var users = {};
-        for (val of data.members) {
-            users[val.id] = val.name;
-        }
-        cb(users);
-    });
+//TODO write one function 'getSlackInfo' that takes a parameter 'Users' or 'Channels'
+botUtil.prototype.getAllUsers = cb => {
+    bot
+        .getUsers()
+        .then(data => {
+            let users = {};
+            for (let val of data.members) {
+                users[val.id] = val.name;
+            }
+            cb(users);
+        });
 }
 
-botUtil.prototype.getAllChannels = (cb) => {
-    bot.getChannels().then(function(data) {
-        var channels = {};
-        for (val of data.channels) {
-            channels[val.id] = val.name;
-        }
-        cb(channels);
-    });
+botUtil.prototype.getAllChannels = cb => {
+    bot
+        .getChannels()
+        .then(data => {
+            let channels = {};
+            for (let val of data.channels) {
+                channels[val.id] = val.name;
+            }
+            cb(channels);
+        });
 }
 
-botUtil.prototype.getAllGroups = (cb) => {
-    bot.getGroups().then(function(data) {
-        var channels = {};
-        for (val of data.channels) {
-            channels[val.id] = val.name;
-        }
-        cb(channels);
-    });
-}
 
 module.exports = botUtil;
